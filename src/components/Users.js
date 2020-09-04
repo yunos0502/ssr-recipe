ec53d074a11cbf510b638688b9b4e3715a4c9934
Park Gyu Ae <yunos0502@gmail.com>
2020-09-04 12:11:26 +0900

    20.4 데이터 로딩 redux-saga

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const Users = ({ users }) => {
   if (!users) return null; // users가 유효하지 않다면 아무것도 보여주지 않음
+
   return (
     <div>
       <ul>
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from "redux";
-import users from "./users";
+import users, { userSaga } from "./users";
+import { all } from "redux-saga/effects";
+
+export function* rootSaga() {
+  yield all([userSaga()]);
+}
 
 const rootReducer = combineReducers({ users });
 export default rootReducer;
